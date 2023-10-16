910289815d64043eef58d24f667d84bd3d56606d
uaquax <uaquax@tutanota.com>
2023-10-16 21:43:07 +0800

    119. Pascal's Triangle II

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 struct Solution;
 
 impl Solution {
-    pub fn is_palindrome(x: i32) -> bool {
-        if x.to_string() == x.to_string().chars().rev().collect::<String>() {
-            return true;
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        if row_index < 1 {
+            return vec![1];
         }
 
-        false
+        let mut result: Vec<Vec<i32>> = vec![];
+
+        for i in 0..row_index + 1 {
+            let mut row = vec![];
+
+            for _ in 0..i + 1 {
+                row.push(0);
+            }
+            result.push(row);
+            for j in 0..i + 1 {
+                if j == 0 || j == i || i < 2 {
+                    result[i as usize][j as usize] = 1;
+                } else {
+                    result[i as usize][j as usize] =
+                        result[i as usize - 1][j as usize] + result[i as usize - 1][j as usize - 1];
+                }
+            }
+        }
+
+        result.pop().unwrap()
     }
 }
 
 fn main() {
-    let result = Solution::is_palindrome(121);
+    let result = Solution::get_row(0);
     println!("Result: {:?}", result);
 }
