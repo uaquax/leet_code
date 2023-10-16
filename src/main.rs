07246d14ce3d2b657a62c188ff944baec2fb3c12
uaquax <uaquax@tutanota.com>
2023-10-16 21:42:51 +0800

    9.  Palindrome Number

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,16 @@
-use std::vec;
-
 struct Solution;
 
 impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for i in 0..nums.len() {
-            for j in i + 1..nums.len() {
-                if nums[i] + nums[j] == target {
-                    return vec![i as i32, j as i32];
-                }
-            }
+    pub fn is_palindrome(x: i32) -> bool {
+        if x.to_string() == x.to_string().chars().rev().collect::<String>() {
+            return true;
         }
 
-        vec![]
+        false
     }
 }
 
 fn main() {
-    let result = Solution::two_sum(vec![3, 2, 3, 6], 6);
+    let result = Solution::is_palindrome(121);
     println!("Result: {:?}", result);
 }
